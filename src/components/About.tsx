51cd7347ccf05e89cd264b59920d2bd8efe6bf27
Sarishkad <sk.sarish.kad@gmail.com>
2026-04-05 20:09:22 +0530

    Initial commit

---
+++ src/components/About.tsx
@@ -0,0 +1,19 @@
+import "./styles/About.css";
+
+const About = () => {
+  return (
+    <div className="about-section" id="about">
+      <div className="about-me">
+        <h3 className="title">About Me</h3>
+        <p className="para">
+          I am a Robotics and Mechatronics Engineer focused on solving real-world challenges by integrating intelligent software with physical systems. My expertise spans the full development pipeline from designing and programming embedded systems using C/C++ on various micro-controller platforms to building scalable robotic architectures with ROS.
+          <br />
+          <br />
+          I specialize in leveraging Python for Computer Vision, Machine Learning, and Deep Learning to develop autonomous systems, including navigation systems, smart surveillance solutions, and drone-based applications. My work is centered on creating innovative, end-to-end robotic solutions that interact dynamically with the real world.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Contact.tsx
@@ -0,0 +1,73 @@
+import { MdArrowOutward, MdCopyright } from "react-icons/md";
+import "./styles/Contact.css";
+
+const Contact = () => {
+  return (
+    <div className="contact-section section-container" id="contact">
+      <div className="contact-container">
+        <h3>Contact</h3>
+        <div className="contact-flex">
+          <div className="contact-box">
+            <h4>Email</h4>
+            <p>
+              <a href="mailto:sk.sarish.kad@gmail.com" data-cursor="disable">
+                sk.sarish.kad@gmail.com
+              </a>
+            </p>
+            <h4>Phone</h4>
+            <p>
+              <a href="tel:+9199999999" data-cursor="disable">
+                +91 99999 99999
+              </a>
+            </p>
+          </div>
+          <div className="contact-box">
+            <h4>Social</h4>
+            <a
+              href="https://github.com/Sarishkad"
+              target="_blank"
+              data-cursor="disable"
+              className="contact-social"
+            >
+              Github <MdArrowOutward />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/sarish-kad-544987252?utm_source=share_via&utm_content=profile&utm_medium=member_ios"
+              target="_blank"
+              data-cursor="disable"
+              className="contact-social"
+            >
+              Linkedin <MdArrowOutward />
+            </a>
+            <a
+              href="https://x.com/sarish_kad?s=11"
+              target="_blank"
+              data-cursor="disable"
+              className="contact-social"
+            >
+              Twitter <MdArrowOutward />
+            </a>
+            <a
+              href="https://www.instagram.com/_sarish_kad?igsh=MWxremIza25hY2dsYQ%3D%3D&utm_source=qr"
+              target="_blank"
+              data-cursor="disable"
+              className="contact-social"
+            >
+              Instagram <MdArrowOutward />
+            </a>
+          </div>
+          <div className="contact-box">
+            <h2>
+              Designed and Developed <br /> by <span>Sarish Kad</span>
+            </h2>
+            <h5>
+              <MdCopyright /> 2026
+            </h5>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
